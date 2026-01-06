20691d1a0b8dc546725834c69c53eeb09d25e166
devhardxd <devhardxd@gmail.com>
2026-01-06 22:38:15 +0700

    Progress : Error Handling

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,14 +1,24 @@
 import { notFound, redirect } from "next/navigation";
 
+function getRandomMath(count: number){
+  return Math.floor(Math.random() * count);
+}
+
 export default async function ProductReview({ 
     params
 }: {
     params: Promise<{ productId: string; reviewId: string }>; 
 }){
-  const { productId, reviewId } = (await params)
+  const random = getRandomMath(2);
+  const { productId, reviewId } = (await params);
+
+  if(random==1){
+    throw new Error("Error Loading Review");
+  }
+
   if(parseInt(reviewId) > 1000){
     // notFound();
-    redirect("/products")
+    redirect("/products");
   }
   return <h1>Review { reviewId } on product { productId }</h1>
 }
