3f968e9fd1c0ba7b8dba9eb984f438e542253583
devhardxd <devhardxd@gmail.com>
2026-01-14 22:21:40 +0700

    Progress: Handling GET and POST Request 2

---
+++ src/app/comments/data.ts
@@ -0,0 +1,14 @@
+export const Comments = [
+    {
+        id: 1,
+        text: "This is the first comment"
+    },
+    {
+        id: 2,
+        text: "This is the first comment"
+    },
+    {
+        id: 3,
+        text: "This is the first comment"
+    },
+]
---
+++ src/app/comments/route.ts
@@ -0,0 +1,18 @@
+import { Comments } from "./data";
+
+export async function GET(){
+    return Response.json(Comments);
+}
+
+export async function POST(request: Request){
+    const comment = await request.json();
+    const newComment = {
+        id: Comments.length + 1,
+        text: comment.text
+    }
+    Comments.push(newComment);
+    return new Response(JSON.stringify(newComment), {
+        headers: {"Content-Type" : "application/json"},
+        status: 201,
+    })
+}
