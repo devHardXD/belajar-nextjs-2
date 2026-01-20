5d5fcbd4dc9477dcec17b21a365e34a20b55de1e
devhardxd <devhardxd@gmail.com>
2026-01-20 22:34:53 +0700

    Progress: Cookies in Route Handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers,cookies } from "next/headers";
 
 export async function GET(
     request: NextRequest,
@@ -7,10 +7,18 @@ export async function GET(
     // const requestHeader = new Headers(request.headers);
     // console.log(requestHeader.get("Authorization"))
     const headerList = await headers();
-    console.log(headerList.get("Authorization"))
+    console.log(headerList.get("Authorization"));
+
+    const theme = request.cookies.get("theme");
+    console.log(theme);
+
+    const cookieStore = await cookies();
+    cookieStore.set("resultPerPage", "20");
+
     return new Response("<h1>Profile API Data</h1>",{
         headers: {
-            "Content-Type": "text/html"
+            "Content-Type": "text/html",
+            "Set-Cookies": "theme=dark"
         }
     })
 }
